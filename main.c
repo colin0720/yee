c9092e6c91bae99c16618c0e8f87be5024fc1075
colin0720 <bwko98@gmail.com>
2018-05-17 11:42:40 +0800

    main

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int ID,Sum; 
+int ID,Sum;/*變數宣告*/ 
 int main(int argc, char *argv[]) {
-	for(ID=0;ID<10;ID++){
-		Sum=Sum+ID; 
+	for(ID=0;ID<10;ID++)/*從0加到9*/
+	{
+		Sum=Sum+ID;/*和*/ 
 	}
-	printf("Sum=%d",Sum); 
+	printf("Sum=%d",Sum);/*印出和*/ 
 		return 0;
 }
